667edebc8c2ecefdedba1d788d314b8781e2fe0b
Andre Moreira Magalhaes <andre@endlessm.com>
2019-08-06 21:05:22 -0300

    Add helper script to run in container
    Example usage:
     $ ./run-in-container.sh src/flatpak-external-data-checker --filter-type=extra-data --update --commit-only path/to/manifest
    
    Signed-off-by: Andre Moreira Magalhaes <andre@endlessm.com>

---
+++ run-in-container.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GIT_USER_NAME=$(git config user.name)
+GIT_USER_EMAIL=$(git config user.email)
+
+user_id_real=$(id -ru 2>/dev/null)
+max_uid_count=65536
+max_minus_uid=$((max_uid_count - user_id_real))
+uid_plus_one=$((user_id_real + 1))
+
+CWD=$(pwd)
+
+podman build -t flatpak-external-data-checker .
+podman run --rm --privileged \
+    -v $HOME:$HOME:rslave \
+    -v $CWD:$CWD:rslave \
+    -w $CWD \
+    -u $(id -u) \
+    --uidmap "$user_id_real":0:1 \
+    --uidmap 0:1:"$user_id_real" \
+    --uidmap "$uid_plus_one":"$uid_plus_one":"$max_minus_uid" \
+    -e GIT_AUTHOR_NAME="$GIT_USER_NAME" \
+    -e GIT_COMMITTER_NAME="$GIT_USER_NAME" \
+    -e GIT_AUTHOR_EMAIL="$GIT_USER_EMAIL" \
+    -e GIT_COMMITTER_EMAIL="$GIT_USER_EMAIL" \
+    -it flatpak-external-data-checker \
+    $*
