flathub-infra/flatpak-external-data-checker

commit b249ee49313b8df83a2dffec4460a46cc60aaa4c

utils: return ExternalFile from get_extra_data_info_from_url()
This function previously returned a tuple with almost all of this
information, just in a different order.

src/checkers/urlchecker.py
@@ -36,7 +36,7 @@ import logging
 import urllib.error
 import re
 
-from lib.externaldata import ExternalData, ExternalFile, Checker
+from lib.externaldata import ExternalData, Checker
 from lib import utils
 
 log = logging.getLogger(__name__)
@@ -70,7 +70,7 @@ class URLChecker(Checker):
         log.debug("Getting extra data info from %s; may take a while", url)
 
         try:
-            new_url, data, checksum, size = utils.get_extra_data_info_from_url(url)
+            new_version, data = utils.get_extra_data_info_from_url(url)
         except urllib.error.HTTPError as e:
             log.warning('%s returned %s', url, e)
             external_data.state = ExternalData.State.BROKEN
@@ -83,19 +83,19 @@ class URLChecker(Checker):
                     external_data.filename, data,
                 )
             elif is_rotating:
-                version_string = extract_version(external_data.checker_data, new_url)
+                version_string = extract_version(
+                    external_data.checker_data,
+                    new_version.url,
+                )
             else:
                 version_string = None
 
             if version_string is not None:
                 log.debug("%s is version %s", external_data.filename, version_string)
+                new_version = new_version._replace(version=version_string)
 
-            new_version = ExternalFile(
-                new_url if is_rotating else url,
-                checksum, size, version_string,
-            )
             if external_data.current_version.matches(new_version):
-                log.debug("URL %s still valid", new_url)
+                log.debug("URL %s still valid", external_data.current_version.url)
                 external_data.state = ExternalData.State.VALID
             else:
                 external_data.state = ExternalData.State.BROKEN

src/checkers/flashchecker.py
@@ -27,7 +27,7 @@ import urllib.error
 import urllib.request
 import urllib.parse
 
-from lib.externaldata import ExternalData, ExternalFile, Checker
+from lib.externaldata import ExternalData, Checker
 from lib import utils
 
 log = logging.getLogger(__name__)
@@ -105,7 +105,7 @@ class FlashChecker(Checker):
         assert latest_url is not None
 
         try:
-            _, _, checksum, size = utils.get_extra_data_info_from_url(latest_url)
+            new_version, _ = utils.get_extra_data_info_from_url(latest_url)
         except urllib.error.HTTPError as e:
             log.warning('%s returned %s', latest_url, e)
             external_data.state = ExternalData.State.BROKEN
@@ -114,6 +114,6 @@ class FlashChecker(Checker):
             external_data.state = ExternalData.State.BROKEN
         else:
             external_data.state = ExternalData.State.VALID
-            new_version = ExternalFile(latest_url, checksum, size, latest_version)
+            new_version = new_version._replace(version=latest_version)
             if not external_data.current_version.matches(new_version):
                 external_data.new_version = new_version

src/lib/utils.py
@@ -27,6 +27,8 @@ import subprocess
 import tempfile
 import urllib.request
 
+from .externaldata import ExternalFile
+
 from gi.repository import GLib
 
 log = logging.getLogger(__name__)
@@ -47,14 +49,16 @@ def get_extra_data_info_from_url(url):
     with urllib.request.urlopen(request, timeout=TIMEOUT_SECONDS) as response:
         real_url = response.geturl()
         data = response.read()
-        size = int(response.info().get('Content-Length', -1))
+        info = response.info()
 
-    if size == -1:
-        size = len(data)
+        if "Content-Length" in info:
+            size = int(info["Content-Length"])
+        else:
+            size = len(data)
 
     checksum = hashlib.sha256(data).hexdigest()
 
-    return real_url, data, checksum, size
+    return ExternalFile(real_url, checksum, size, None), data
 
 
 def extract_appimage_version(basename, data):

commit cb15aeb145b98aba13003ffac393a8a2a3af9f5c

urlchecker: don't follow redirects for non-rotating URLs
b249ee4 introduced a regression: if the URL is stable, but happens to
be a redirect (to a CDN, for example), the checker would update it to
the CDN URL. See
https://github.com/flathub/com.visualstudio.code/pull/99 for an example
of this happening in practice.

Fix this by restoring the old behaviour of only using the post-redirect
URL if is_rotating is true. I think the logic is a little hard to follow
now, but I'm not sure how to write it more cleanly.

Add tests for both the rotating and the non-rotating cases.

src/checkers/urlchecker.py
@@ -94,6 +94,9 @@ class URLChecker(Checker):
                 log.debug("%s is version %s", external_data.filename, version_string)
                 new_version = new_version._replace(version=version_string)
 
+            if not is_rotating:
+                new_version = new_version._replace(url=url)
+
             if external_data.current_version.matches(new_version):
                 log.debug("URL %s still valid", external_data.current_version.url)
                 external_data.state = ExternalData.State.VALID

tests/test_checker.py
@@ -34,7 +34,7 @@ from checker import ManifestChecker
 TEST_MANIFEST = os.path.join(tests_dir, 'org.externaldatachecker.Manifest.json')
 NUM_ARCHIVE_IN_MANIFEST = 1
 NUM_FILE_IN_MANIFEST = 1
-NUM_EXTRA_DATA_IN_MANIFEST = 6
+NUM_EXTRA_DATA_IN_MANIFEST = 7
 NUM_ALL_EXT_DATA = NUM_ARCHIVE_IN_MANIFEST + NUM_FILE_IN_MANIFEST + \
                    NUM_EXTRA_DATA_IN_MANIFEST
 
@@ -219,14 +219,27 @@ modules:
         self.assertEqual(len(extra_data), NUM_EXTRA_DATA_IN_MANIFEST)
 
         outdated_ext_data = checker.get_outdated_external_data()
-        self.assertEqual(len(outdated_ext_data), NUM_ALL_EXT_DATA)
+        self.assertEqual(len(outdated_ext_data), NUM_ALL_EXT_DATA - 1)
 
+        dropbox = self._find_by_filename(ext_data, "dropbox.tgz")
+        self.assertIsNotNone(dropbox)
+        self.assertEqual(dropbox.new_version.version, "1.2.3.4")
+        self.assertEqual(
+            dropbox.new_version.url,
+            "https://httpbin.org/image/jpeg?version=1.2.3.4",
+        )
+
+        # this URL is a redirect, but since it is not a rotating-url the URL
+        # should not be updated.
+        image = self._find_by_filename(ext_data, "image.jpeg")
+        self.assertIsNone(image)
+
+    def _find_by_filename(self, ext_data, filename):
         for data in ext_data:
-            if data.filename == "dropbox.tgz":
-                self.assertEqual(data.new_version.version, "1.2.3.4")
-                break
+            if data.filename == filename:
+                return data
         else:
-            self.fail("dropbox.tgz data not found")
+            return None
 
 
 if __name__ == '__main__':