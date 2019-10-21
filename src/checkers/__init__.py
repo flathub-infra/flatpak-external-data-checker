9e7fc3c278a520f31cdf53b4690e4856b0d50ee9
Will Thompson <wjt@endlessm.com>
2019-10-21 06:53:00 +0100

    checkers: list URLChecker last
    No checker listed after URLChecker will ever be run. Document that the
    order is significant and fix the order.

--- src/checkers/__init__.py
+++ src/checkers/__init__.py
@@ -5,10 +5,12 @@ from .urlchecker import URLChecker
 from .htmlchecker import HTMLChecker
 
 
+# For each ExternalData, checkers are run in the order listed here, stopping once data.state is
+# set to something other than UNKNOWN.
 ALL_CHECKERS = [
     DebianRepoChecker,
     FirefoxChecker,
     FlashChecker,
-    URLChecker,
-    HTMLChecker
+    HTMLChecker,
+    URLChecker,  # leave this last
 ]
