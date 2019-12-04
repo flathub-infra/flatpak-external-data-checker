f5b053694ad824b851533c99eb8c4312f37d66d3
Will Thompson <will@willthompson.co.uk>
2019-12-04 20:33:58 +0000

    firefoxchecker: HEAD, don't GET, each file
    We get the list of files from a file called SHA256SUMS, which
    unsurprisingly contains the SHA256 of each file. So we don't need to
    fetch them: we just need to HEAD them to determine their Content-Length
    and timestamp.
    
    On my machine & network, test_firefoxchecker used to take 2m28s; it now
    takes 16s.

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -76,6 +76,25 @@ def get_timestamp_from_url(url):
     wait=wait_fixed(2),
     before_sleep=before_sleep_log(log, logging.DEBUG),
 )
+def get_extra_data_info_from_head(url):
+    request = urllib.request.Request(url, headers=HEADERS, method="HEAD")
+
+    with urllib.request.urlopen(request, timeout=TIMEOUT_SECONDS) as response:
+        real_url = response.geturl()
+        info = response.info()
+        size = int(info["Content-Length"])
+
+    return ExternalFile(
+        real_url, None, size, None, _extract_timestamp(info),
+    )
+
+
+@retry(
+    retry=retry_if_exception_type((ConnectionResetError, socket.timeout)),
+    stop=stop_after_attempt(3),
+    wait=wait_fixed(2),
+    before_sleep=before_sleep_log(log, logging.DEBUG),
+)
 def get_extra_data_info_from_url(url):
     request = urllib.request.Request(url, headers=HEADERS)
     data = None
