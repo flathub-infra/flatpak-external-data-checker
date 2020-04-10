80eb856c7f23846c2d74b66676f50d0edb5c9ce3
Rebecca Wallander <sakcheen@gmail.com>
2020-04-10 16:11:50 +0200

    Calculate sha256 if url is new

--- src/checkers/snapcraftchecker.py
+++ src/checkers/snapcraftchecker.py
@@ -2,6 +2,8 @@ import urllib.request
 import datetime
 import json
 import logging
+import os
+import hashlib
 
 from src.lib.externaldata import ExternalFile, Checker
 
@@ -9,11 +11,28 @@ log = logging.getLogger(__name__)
 
 
 class SnapcraftChecker(Checker):
-    arches = {"x86_64": "amd64", "aarch64": "arm64", "arm": "armhf", "i386": "i386"}
+    _arches = {"x86_64": "amd64", "aarch64": "arm64", "arm": "armhf", "i386": "i386"}
+    _BLOCK_SIZE = 65536
 
     @staticmethod
     def _should_check(external_data):
         return external_data.checker_data.get("type") == "snapcraft"
+
+    def _get_sha256(self, url: str, sha3_384: str):
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+
+        sha2 = hashlib.sha256()
+        sha3 = hashlib.sha3_384()
+
+        data = resp.read(self._BLOCK_SIZE)
+        while data:
+            sha2.update(data)
+            sha3.update(data)
+            data = resp.read(self._BLOCK_SIZE)
+
+        if sha3.hexdigest() == sha3_384:
+            return sha2.hexdigest()
 
     def check(self, external_data):
         if not self._should_check(external_data):
@@ -28,27 +47,29 @@ class SnapcraftChecker(Checker):
 
         req = urllib.request.Request(url, headers=header)
 
-        log.debug("Getting extra data info from %s; may take a while", url)
+        log.debug("Getting extra data info from %s", url)
         resp = urllib.request.urlopen(req)
 
-        d = json.load(resp)
+        js = json.load(resp)
 
-        data = [
-            x
-            for x in d["channel-map"]
-            if x["channel"]["architecture"] == self.arches[external_data.arches[0]]
-            and x["channel"]["name"] == channel
-        ][0]
+        data = [x for x in js["channel-map"] if
+                x["channel"]["architecture"] == self._arches[external_data.arches[0]] and x["channel"][
+                    "name"] == channel][0]
 
-        new_version = ExternalFile(
-            data["download"]["url"],
-            data["download"]["sha3-384"],
-            data["download"]["size"],
-            data["version"],
-            datetime.datetime.strptime(
-                data["channel"]["released-at"], "%Y-%m-%dT%H:%M:%S.%f%z"
-            ),
-        )
+        if external_data.current_version.url != data["download"]["url"]:
+            log.debug("Downloading file from %s; may take a while", url)
+            sha256 = self._get_sha256(data["download"]["url"], data["download"]["sha3-384"])
 
-        if not external_data.current_version.matches(new_version):
-            external_data.new_version = new_version
+            if sha256:
+                new_version = ExternalFile(
+                    data["download"]["url"],
+                    sha256,
+                    data["download"]["size"],
+                    data["version"],
+                    datetime.datetime.strptime(
+                        data["channel"]["released-at"], "%Y-%m-%dT%H:%M:%S.%f%z"
+                    ),
+                )
+
+                if not external_data.current_version.matches(new_version):
+                    external_data.new_version = new_version
