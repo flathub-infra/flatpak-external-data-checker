064fdeaa85e00bf72832d1991a571b4e009137ae
gasinvein <gasinvein@gmail.com>
2021-01-19 13:24:18 +0300

    Adapt docker image for local use

--- run-in-container.sh
+++ run-in-container.sh
@@ -4,11 +4,6 @@ set -e
 GIT_USER_NAME=$(git config user.name)
 GIT_USER_EMAIL=$(git config user.email)
 
-user_id_real=$(id -ru 2>/dev/null)
-max_uid_count=65536
-max_minus_uid=$((max_uid_count - user_id_real))
-uid_plus_one=$((user_id_real + 1))
-
 CWD=$(pwd)
 HERE=$( dirname "${BASH_SOURCE[0]}" )
 
@@ -17,10 +12,6 @@ podman run --rm --privileged \
     -v $HOME:$HOME:rslave \
     -v $CWD:$CWD:rslave \
     -w $CWD \
-    -u $(id -u) \
-    --uidmap "$user_id_real":0:1 \
-    --uidmap 0:1:"$user_id_real" \
-    --uidmap "$uid_plus_one":"$uid_plus_one":"$max_minus_uid" \
     -e GIT_AUTHOR_NAME="$GIT_USER_NAME" \
     -e GIT_COMMITTER_NAME="$GIT_USER_NAME" \
     -e GIT_AUTHOR_EMAIL="$GIT_USER_EMAIL" \
