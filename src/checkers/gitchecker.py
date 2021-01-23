93eac7b7bbb9e17c62ed47832974864c685c5de2
gasinvein <gasinvein@gmail.com>
2021-01-23 19:16:11 +0300

    Add git checker This is similar to URL checker - it applies to all git sources and checks if the specified tag/branch is still valid

---
+++ src/checkers/gitchecker.py
@@ -0,0 +1,37 @@
+import logging
+import subprocess
+from src.lib.externaldata import Checker, ExternalGitRepo
+
+log = logging.getLogger(__name__)
+
+
+class GitChecker(Checker):
+    CHECKER_DATA_TYPE = "git"
+    SUPPORTED_DATA_CLASSES = [ExternalGitRepo]
+
+    def should_check(self, external_data):
+        return isinstance(external_data, ExternalGitRepo)
+
+    def check(self, external_data):
+        assert self.should_check(external_data)
+        external_data: ExternalGitRepo
+
+        try:
+            remote_version = external_data.current_version.fetch_remote()
+        except KeyError as err:
+            log.error(
+                "Couldn't get remote commit from %s: not found %s",
+                external_data.current_version.url,
+                err,
+            )
+            external_data.state = ExternalGitRepo.State.BROKEN
+            return
+
+        if external_data.current_version.matches(remote_version):
+            log.debug(
+                "Remote git repo %s is still valid", external_data.current_version.url
+            )
+            external_data.state = ExternalGitRepo.State.VALID
+        else:
+            external_data.state = ExternalGitRepo.State.BROKEN
+            external_data.new_version = remote_version
