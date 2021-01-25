61ce5a2ab773996a0ae14d1a5827645d43568919
gasinvein <gasinvein@gmail.com>
2021-01-25 20:47:33 +0300

    Update test for anitya to include git sources

--- tests/test_anytiachecker.py
+++ tests/test_anytiachecker.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from src.checker import ManifestChecker
+from src.lib.externaldata import ExternalFile, ExternalGitRef
 from src.lib.utils import init_logging
 
 TEST_MANIFEST = os.path.join(os.path.dirname(__file__), "org.flatpak.Flatpak.yml")
@@ -15,21 +16,34 @@ class TestAnityaChecker(unittest.TestCase):
         checker = ManifestChecker(TEST_MANIFEST)
         ext_data = checker.check()
 
-        self.assertEqual(len(ext_data), 1)
-        data = ext_data[0]
-        self.assertIsNotNone(data.new_version)
-        self.assertRegex(
-            data.new_version.url,
-            r"^https://github.com/flatpak/flatpak/releases/download/[\w\d.]+/flatpak-[\w\d.]+.tar.xz$",
-        )
-        self.assertIsInstance(data.new_version.size, int)
-        self.assertGreater(data.new_version.size, 0)
-        self.assertIsNotNone(data.new_version.checksum)
-        self.assertIsInstance(data.new_version.checksum, str)
-        self.assertNotEqual(
-            data.new_version.checksum,
-            "7926625df7c2282a5ee1a8b3c317af53d40a663b1bc6b18a2dc8747e265085b0",
-        )
+        self.assertEqual(len(ext_data), 2)
+        for data in ext_data:
+            if data.filename == "flatpak-1.8.2.tar.xz":
+                self.assertIsNotNone(data.new_version)
+                self.assertIsInstance(data.new_version, ExternalFile)
+                self.assertRegex(
+                    data.new_version.url,
+                    r"^https://github.com/flatpak/flatpak/releases/download/[\w\d.]+/flatpak-[\w\d.]+.tar.xz$",
+                )
+                self.assertIsInstance(data.new_version.size, int)
+                self.assertGreater(data.new_version.size, 0)
+                self.assertIsNotNone(data.new_version.checksum)
+                self.assertIsInstance(data.new_version.checksum, str)
+                self.assertNotEqual(
+                    data.new_version.checksum,
+                    "7926625df7c2282a5ee1a8b3c317af53d40a663b1bc6b18a2dc8747e265085b0",
+                )
+            elif data.filename == "ostree.git":
+                self.assertIsNotNone(data.new_version)
+                self.assertIsInstance(data.new_version, ExternalGitRef)
+                self.assertIsNotNone(data.new_version.version)
+                self.assertIsNotNone(data.new_version.tag)
+                self.assertNotEqual(data.new_version.tag, data.current_version.tag)
+                self.assertNotEqual(
+                    data.new_version.commit, data.current_version.commit
+                )
+            else:
+                self.fail(f"Unknown data {data.filename}")
 
 
 if __name__ == "__main__":
