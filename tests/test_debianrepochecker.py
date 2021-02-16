9f0ed1afa8fe92ee8300901cb24224556161dd79
gasinvein <gasinvein@gmail.com>
2021-02-17 00:19:39 +0300

    tests: Split debian repo checker test

---
+++ tests/test_debianrepochecker.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+
+from src.checker import ManifestChecker
+from src.lib.utils import init_logging
+
+TEST_MANIFEST = os.path.join(
+    os.path.dirname(__file__), "org.debian.tracker.pkg.apt.yml"
+)
+
+
+class TestDebianRepoChecker(unittest.TestCase):
+    def setUp(self):
+        init_logging()
+
+    def test_check(self):
+        checker = ManifestChecker(TEST_MANIFEST)
+        ext_data = checker.check()
+        for data in ext_data:
+            self.assertIsNotNone(data)
+            self.assertIsNotNone(data.new_version)
+            self.assertIsNotNone(data.new_version.url)
+            self.assertIsNotNone(data.new_version.checksum)
+            self.assertIsNotNone(data.new_version.version)
+            self.assertNotEqual(data.new_version.url, data.current_version.url)
+            self.assertNotEqual(
+                data.new_version.checksum, data.current_version.checksum
+            )
+            self.assertRegex(
+                data.new_version.url, r"http://deb.debian.org/debian/pool/main/.+"
+            )
