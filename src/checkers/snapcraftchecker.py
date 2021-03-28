f6fb5ba65b8c5f1476de7c865357c3f0b8be37a9
gasinvein <gasinvein@gmail.com>
2021-03-28 21:28:26 +0300

    snapcraftchecker: Make network IO async

--- src/checkers/snapcraftchecker.py
+++ src/checkers/snapcraftchecker.py
@@ -1,6 +1,4 @@
-import urllib.request
 import datetime
-import json
 import logging
 import hashlib
 
@@ -15,18 +13,15 @@ class SnapcraftChecker(Checker):
     _arches = {"x86_64": "amd64", "aarch64": "arm64", "arm": "armhf", "i386": "i386"}
     _BLOCK_SIZE = 65536
 
-    def _get_sha256(self, url: str, sha3_384: str):
-        req = urllib.request.Request(url)
-        resp = urllib.request.urlopen(req)
-
+    async def _get_sha256(self, url: str, sha3_384: str):
+        assert self.session is not None
         sha2 = hashlib.sha256()
         sha3 = hashlib.sha3_384()
 
-        data = resp.read(self._BLOCK_SIZE)
-        while data:
-            sha2.update(data)
-            sha3.update(data)
-            data = resp.read(self._BLOCK_SIZE)
+        async with self.session.get(url) as response:
+            async for data in response.content.iter_chunked(self._BLOCK_SIZE):
+                sha2.update(data)
+                sha3.update(data)
 
         if sha3.hexdigest() == sha3_384:
             return sha2.hexdigest()
@@ -40,12 +35,8 @@ class SnapcraftChecker(Checker):
         url = f"http://api.snapcraft.io/v2/snaps/info/{name}"
         header = {"Snap-Device-Series": "16"}
 
-        req = urllib.request.Request(url, headers=header)
-
-        log.debug("Getting extra data info from %s", url)
-        resp = urllib.request.urlopen(req)
-
-        js = json.load(resp)
+        async with self.session.get(url, headers=header) as response:
+            js = await response.json()
 
         data = [
             x
@@ -56,7 +47,7 @@ class SnapcraftChecker(Checker):
 
         if external_data.current_version.url != data["download"]["url"]:
             log.debug("Downloading file from %s; may take a while", url)
-            sha256 = self._get_sha256(
+            sha256 = await self._get_sha256(
                 data["download"]["url"], data["download"]["sha3-384"]
             )
 
