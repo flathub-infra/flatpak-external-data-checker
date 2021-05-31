af2dbb06decdece86560180a778d6c85e69848dd
Ryan Gonzalez <rymg19@gmail.com>
2021-05-30 21:36:55 -0500

    chromiumchecker: Add checker for Chromium components
    This is needed to be able to auto-update the versions for the LLVM
    toolchain components in sync with Chromium itself. It also can update
    the Chromium tarball; the Anitya checker could already technically do
    this, but this code needs to parse the versions anyway, so actually
    updating the tarball in sync is a minor addition.

---
+++ tests/test_chromiumchecker.py
@@ -0,0 +1,66 @@
+import logging
+import os
+import unittest
+from distutils.version import LooseVersion
+
+from src.checker import ManifestChecker
+from src.lib.externaldata import (
+    ExternalDataSource,
+    ExternalFile,
+    ExternalGitRef,
+    ExternalGitRepo,
+)
+from src.lib.utils import init_logging
+
+TEST_MANIFEST = os.path.join(os.path.dirname(__file__), "org.chromium.Chromium.yaml")
+
+
+class TestChromiumChecker(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        init_logging(logging.INFO)
+
+    async def test_check(self):
+        checker = ManifestChecker(TEST_MANIFEST)
+        ext_data = await checker.check()
+
+        self.assertEqual(len(ext_data), 3)
+        for data in ext_data:
+            self.assertIsNotNone(data.new_version)
+            self.assertIsNotNone(data.new_version.version)
+            self.assertGreater(
+                LooseVersion(data.new_version.version), LooseVersion("90.0.4430.212")
+            )
+
+            if isinstance(data, ExternalDataSource):
+                self.assertIsInstance(data.new_version, ExternalFile)
+                self.assertIsNotNone(data.new_version.checksum)
+                self.assertIsInstance(data.new_version.checksum, str)
+                if data.filename.startswith("chromium-"):
+                    self.assertRegex(
+                        data.new_version.url,
+                        r"^https://commondatastorage.googleapis.com/chromium-browser-official/chromium-[\d.]+\.tar\.xz$",
+                    )
+                    self.assertNotEqual(
+                        data.new_version.checksum,
+                        "abe11d0cb1ff21278aad2eec1a1e279d59176b15331804d7df1807446786d59e",
+                    )
+                elif data.filename.startswith("clang-"):
+                    self.assertRegex(
+                        data.new_version.url,
+                        r"^https://commondatastorage.googleapis.com/chromium-browser-clang/Linux_x64/clang-.*\.tgz$",
+                    )
+                    self.assertNotEqual(
+                        data.new_version.checksum,
+                        "676448e180fb060d3983f24476a2136eac83c6011c600117686035634a2bbe26",
+                    )
+                else:
+                    self.fail(f"unexpected extra-data filename {data.filename}")
+            elif isinstance(data, ExternalGitRepo):
+                self.assertEqual(data.filename, "llvm-project")
+                self.assertIsInstance(data.new_version, ExternalGitRef)
+                self.assertIsNotNone(data.new_version.commit)
+                self.assertNotEqual(
+                    data.new_version.commit, data.current_version.commit
+                )
+            else:
+                self.fail(repr(type(data)))
