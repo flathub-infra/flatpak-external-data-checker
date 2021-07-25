987b7e02c9d2f5dc4c06f23835d1878e1fbf4aea
gasinvein <gasinvein@gmail.com>
2021-07-25 17:37:24 +0300

    tests: pypichecker: Assert checksum type

--- tests/test_pypichecker.py
+++ tests/test_pypichecker.py
@@ -23,6 +23,7 @@ class TestPyPIChecker(unittest.IsolatedAsyncioTestCase):
                 self.assertIsNotNone(data.new_version.checksum)
                 self.assertIsNotNone(data.new_version.version)
                 self.assertNotEqual(data.new_version.url, data.current_version.url)
+                self.assertIsInstance(data.new_version.checksum, str)
                 self.assertNotEqual(
                     data.new_version.checksum, data.current_version.checksum
                 )
