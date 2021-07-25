14bc7eb8646d6fdcef7211bf06720e26d56920c5
gasinvein <gasinvein@gmail.com>
2021-07-25 22:22:18 +0300

    rpmrepochecker: Adapt for multiple digests

--- tests/test_rpmrepochecker.py
+++ tests/test_rpmrepochecker.py
@@ -3,6 +3,7 @@ import os
 
 from src.checker import ManifestChecker
 from src.lib.utils import init_logging
+from src.lib.checksums import MultiDigest
 
 TEST_MANIFEST = os.path.join(os.path.dirname(__file__), "com.visualstudio.code.yaml")
 
@@ -21,7 +22,7 @@ class TestRPMRepoChecker(unittest.IsolatedAsyncioTestCase):
             self.assertIsNotNone(data.new_version.checksum)
             self.assertIsNotNone(data.new_version.version)
             self.assertNotEqual(data.new_version.url, data.current_version.url)
-            self.assertIsInstance(data.new_version.checksum, str)
+            self.assertIsInstance(data.new_version.checksum, MultiDigest)
             self.assertNotEqual(
                 data.new_version.checksum, data.current_version.checksum
             )
