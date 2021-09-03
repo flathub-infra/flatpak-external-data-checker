a2adb5af7fb9a0fff67531132399e46410fe9996
gasinvein <gasinvein@gmail.com>
2021-09-03 19:33:28 +0300

    anityachecker: Add more type hints
    And use yarl instead of urllib

--- src/checkers/anityachecker.py
+++ src/checkers/anityachecker.py
@@ -1,9 +1,14 @@
 import logging
-import urllib.request
-import urllib.parse
+
+from yarl import URL
 
 from ..lib import OPERATORS_SCHEMA
-from ..lib.externaldata import ExternalData, ExternalGitRepo, ExternalGitRef
+from ..lib.externaldata import (
+    ExternalBase,
+    ExternalData,
+    ExternalGitRepo,
+    ExternalGitRef,
+)
 from ..lib.utils import filter_versions
 from .htmlchecker import HTMLChecker
 
@@ -29,18 +34,18 @@ class AnityaChecker(HTMLChecker):
     }
     SUPPORTED_DATA_CLASSES = [ExternalData, ExternalGitRepo]
 
-    async def check(self, external_data):
+    async def check(self, external_data: ExternalBase):
         assert self.should_check(external_data)
 
         instance_url = external_data.checker_data.get(
             "baseurl", "https://release-monitoring.org"
         )
-        versions_url = urllib.request.urljoin(instance_url, "/api/v2/versions/")
+        versions_url = URL(instance_url) / "api/v2/versions/"
         stable_only = external_data.checker_data.get("stable-only", False)
         constraints = external_data.checker_data.get("versions", {}).items()
 
-        query = {"project_id": external_data.checker_data.get("project-id")}
-        async with self.session.get(versions_url, params=query) as response:
+        query = {"project_id": external_data.checker_data["project-id"]}
+        async with self.session.get(versions_url % query) as response:
             result = await response.json()
 
         if stable_only or constraints:
@@ -56,9 +61,10 @@ class AnityaChecker(HTMLChecker):
 
         if isinstance(external_data, ExternalGitRepo):
             return await self._check_git(external_data, latest_version)
+        assert isinstance(external_data, ExternalData)
         return await self._check_data(external_data, latest_version)
 
-    async def _check_data(self, external_data, latest_version):
+    async def _check_data(self, external_data: ExternalData, latest_version):
         url_template = external_data.checker_data["url-template"]
         latest_url = self._substitute_placeholders(url_template, latest_version)
 
@@ -66,7 +72,7 @@ class AnityaChecker(HTMLChecker):
             external_data, latest_version, latest_url, follow_redirects=False
         )
 
-    async def _check_git(self, external_data, latest_version):
+    async def _check_git(self, external_data: ExternalGitRepo, latest_version):
         tag_template = external_data.checker_data["tag-template"]
         latest_tag = self._substitute_placeholders(tag_template, latest_version)
 
