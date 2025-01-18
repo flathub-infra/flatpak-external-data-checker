1e1cdab549f3f6844c84f09781db84592b97c34f
bbhtt <bbhtt.zn0i8@slmail.me>
2025-01-18 07:10:39 +0530

    tests: Fix boost source and x-checker data
    jfrog now goes to a landing page, https://www.boost.org/users/download/
    points to archives.boost.io instead

--- tests/test_anityachecker.py
+++ tests/test_anityachecker.py
@@ -46,7 +46,7 @@ class TestAnityaChecker(unittest.IsolatedAsyncioTestCase):
                 self.assertIsInstance(data.new_version, ExternalFile)
                 self.assertRegex(
                     data.new_version.url,
-                    r"^https://boostorg\.jfrog\.io/artifactory/main/release/[\d.]+/source/boost_[\d]+_[\d]+_[\d]+.tar.bz2$",  # noqa: E501
+                    r"^https://archives\.boost\.io/release/[\d.]+/source/boost_[\d]+_[\d]+_[\d]+.tar.bz2$",  # noqa: E501
                 )
                 self.assertIsNotNone(data.new_version.version)
                 self.assertGreater(
