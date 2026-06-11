ff5241e1289d1b818d3ec9e3d06ce5afff0fa571
bbhtt <bbhtt.zn0i8@slmail.me>
2026-06-11 10:19:23 +0530

    tests: Drop shebang from test files

--- tests/test_gnomechecker.py
+++ tests/test_gnomechecker.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright © 2020–2021 Maximiliano Sandoval <msandova@protonmail.com>
 #
 # Authors:
--- tests/test_newlinepreservation.py
+++ tests/test_newlinepreservation.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright (C) 2021 Carles Pastor Badosa
 #
 # Authors:
